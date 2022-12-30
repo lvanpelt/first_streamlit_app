dc2b9148244dd32ba48478a6658e59e7f90f7fcc
lvanpelt <57357176+lvanpelt@users.noreply.github.com>
2022-12-30 13:41:42 -0600

    Update streamlit_app.py
    updated menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Parents Healthy Diner Menu')
+streamlit.header('Breakfast Menu')
+streamlit.text('Sausage and Eggs')
+streamlit.text('Kale Smoothie')
+streamlit.text('Biscuits and Gravy')
